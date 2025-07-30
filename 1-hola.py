0268deba0ecacae19a0b6a58ec31a2d74354bf4b
japinuela <juan.pinuela@universidadeuropea.es>
2025-07-30 18:56:01 +0200

    Ejercicios Curso PythonML

---
+++ 1-hola.py
@@ -0,0 +1,6 @@
+'''
+1.	Escribe un programa que use input para preguntarle al usuario su nombre y 
+le dé la bienvenida con “Hola, Juan” usando print.
+'''
+nombre = input("¿Cuál es tu nombre? ")
+print("Hola, " + nombre)
