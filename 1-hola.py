a2a88c7e867849c767b500cad66018397360cde0
japinuela <juan.pinuela@universidadeuropea.es>
2025-07-30 19:06:51 +0200

    Ejercicio 1

--- 1-hola.py
+++ 1-hola.py
@@ -2,5 +2,27 @@
 1.	Escribe un programa que use input para preguntarle al usuario su nombre y 
 le dé la bienvenida con “Hola, Juan” usando print.
 '''
+print("#"*100)
 nombre = input("¿Cuál es tu nombre? ")
-print("Hola, " + nombre)
+print("Hola, " , nombre)
+
+'''
+f-strings
+'''
+
+
+print("-"*100)
+print(f'Hola, {nombre.upper()}!')
+
+
+print("-"*100)
+
+precio = 45.6787787
+print(f'El precio es {precio:.2f} €')
+print(f'El precio es {precio:10.2f} €')  # 10 espacios, 2 decimales
+
+
+print("-"*100)
+
+persona = {"nombre": "Juan Antonio", "edad": 30}
+print(f'Hola, {persona["nombre"]} tienes {persona["edad"]} años')
